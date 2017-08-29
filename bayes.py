93159a0454e9dc82bee261d756f45c1df1af1ea9
ZLJ <3977270@qq.com>
2017-08-29 15:14:36 +0800

    提交

--- bayes.py
+++ bayes.py
@@ -1,4 +1,6 @@
+#coding:utf-8
 def loadDataSet():
+    #记录
     postingList=[['my','dog','has','flea','problems','help','please'],
                  ['maybe','not','take','him','to','dog','park','stupid'],
                  ['my','dalmation','is','so','cute','I','love','him'],
@@ -8,3 +10,9 @@ def loadDataSet():
                  ]
     classVec=[0,1,0,1,0,1]
     return postingList,classVec
+def createVocabList(dataSet):
+    #生词
+    vocabSet=set([])
+    for document in dataSet:
+        vocabSet=vocabSet|set(document)
+    return list(vocabSet)
